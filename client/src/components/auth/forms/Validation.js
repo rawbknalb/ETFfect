bc1e18e1ce4e94232ef89101b2ce0e124516240a
rawbknalb <rawbknalb@gmail.com>
2017-02-16 16:47:35 +0100

    Update Validation.js

--- client/src/components/auth/forms/Validation.js
+++ client/src/components/auth/forms/Validation.js
@@ -3,21 +3,21 @@ export const validate = values => {
   const errors = {};
   const {email, password, passwordConfirm} = values;
   if (!values.email) {
-    errors.email = "Required";
+    errors.email = "Please enter an email";
   } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
     errors.email = "Invalid email address";
   }
   if (!values.password) {
-    errors.password = "Required";
+    errors.password = "Please enter a password";
   } else if (values.password.length < 8) {
     errors.password = "Must be 8 characters or less";
   }
   if (!values.passwordConfirm) {
-    errors.passwordConfirm = "Required";
+    errors.passwordConfirm = "Please enter a password confirmation";
   } else if (values.passwordConfirm.length < 8) {
     errors.passwordConfirm = "Must be 8 characters or less";
   }
-  if(password !== passwordConfirm) {
+  if(password && passwordConfirm && password !== passwordConfirm) {
     errors.password = "Passwords do not match";
   }
   return errors;
